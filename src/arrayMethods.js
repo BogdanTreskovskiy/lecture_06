e3c94834b86a5ec8fc052b8c399774992e7f9a3c
olexandervdovenko <callmetraitorwot@gmail.com>
2018-05-03 18:31:04 +0300

    implement forEach

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,5 +1,9 @@
 window.every = function () {};
 window.some = function () {};
-window.forEach = function () {};
+window.forEach = function (array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
+};
 window.filter = function () {};
 window.map = function () {};
